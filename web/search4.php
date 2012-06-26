f9effa56109bec16afd1d098c0cac218f03cc40b
unknown <WIP@Cythrawl.(none)>
2012-06-27 00:12:50 +0200

    Aenderungen vom Mittag an Michaels Webgui-Sachen. Fast vollstaendiges XSLT (bis auf Punktstrings trennen). Ersten 2 tracks aus aktuellstem gpsies crawl in crawl-db2-start.xml

---
+++ web/search4.php
@@ -0,0 +1,95 @@
+<?php
+/*
+* This example shows how database commands can be executed
+* and the result of a query can be processed in a dom document.
+*
+* Documentation: http://docs.basex.org/wiki/Clients
+*
+* (C) BaseX Team 2005-12, BSD License
+*/
+include("BaseXClient.php");
+
+
+
+$searchIn =  $_POST["searchInput"];
+//$trackType =  $_POST["check"];
+
+try {
+  // initialize timer
+  $start = microtime(true);
+
+  // var from HTML
+  
+  
+  // create session
+  $session = new Session("localhost", 1984, "admin", "admin");
+    
+  // dom document
+  //$dom = new DOMDocument();
+
+
+  // open test.xml
+  $session->execute("CREATE DB db-crawl db-crawl.xml");
+  $session->execute("OPEN db-crawl");
+  // query
+	$input = 'declare variable $search external;
+	let $my-doc := doc("db-crawl.xml")
+	return
+	<html>
+		<head>
+			<title>Tracks</title>
+			<!--<script src="test.js" type="text/javascript" />-->
+		</head>
+		<body>
+		<table border="1">
+		<thead>
+		  <tr>
+			  <th>Track Title</th> 
+			  <th>CreateDate</th>
+			  <th>TrackType</th>
+		  </tr>
+		</thead>
+		<tbody>{
+			
+		   for $term at $count in 
+			 for $item in $my-doc//tracks/track
+				 let $track-name := $item/title/text()'.
+				 //let $track-type := $item/trackTypes/trackType/*
+				 'where contains(upper-case($track-name), upper-case($search))'. //and contains($track-type, "biking")
+				 
+				'order by upper-case($track-name)
+			 return $item
+		   return
+		   	 <tr> {if ($count mod 2) then (attribute bgcolor {"Lightblue"}) else ()}
+			   <td>{$term/title/text()}</td>
+			   <td>{$term/createdDate/text()}</td>
+			   <td>{$term/trackTypes/node()}</td>
+			 </tr>
+			 
+		   }		   
+		   </tbody>
+		 </table>
+	   </body>
+	</html>';
+  $query = $session->query($input);
+  $query->bind("search",$searchIn);
+  print $query->execute()."<br/>";
+  $query->close();
+  
+  $session->execute("DROP DB db-crawl");
+  
+  // close session
+  $session->close();
+  
+  // print time needed
+  $time = (microtime(true) - $start) * 1000;
+  print "<br/>$time ms\n";
+
+
+} catch (Exception $e) {
+  // print exception
+  print $e->getMessage();
+}
+
+
+?>
